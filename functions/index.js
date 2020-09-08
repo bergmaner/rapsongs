afb78066088e5546744b8a57ce3286c2230a8119
bergmaner <bergkacper@gmail.com>
2020-09-08 14:28:08 +0200

    comments validate

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,43 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions")
+const admin = require("firebase-admin")
 
-admin.initializeApp();
+admin.initializeApp()
 
-exports.postComment = functions.https.onCall(async(data, context) => {
-const db = admin.firestore();
-const snapshot = await db
-.collection('profiles')
-.where('userId','==',context.auth.uid)
-.limit(1)
-.get();
-  await db.collection('comments').add({
+exports.postComment = functions.https.onCall(async (data, context) => {
+  checkAuthentication(context);
+  dataValidate(data,{
+    albumId: 'string',
+    content: 'string'
+  })
+
+  const db = admin.firestore()
+  const snapshot = await db
+    .collection("profiles")
+    .where("userId", "==", context.auth.uid)
+    .limit(1)
+    .get()
+  await db.collection("comments").add({
     content: data.content,
     username: snapshot.docs[0].id,
     dateCreated: new Date(),
-    album: db.collection('albums').doc(data.albumId)
+    album: db.collection("albums").doc(data.albumId),
+  })
 })
-});
+
+function dataValidate(data,validKeys){
+  if(Object.keys(data).length !== Object.keys(validKeys).length){
+    throw new functions.https.HttpsError('invalid-argument', 'Invalid number of properties');
+  }
+  else{
+    for(let key in data){
+      if(!validKeys[key] || typeof(data[key]) !== validKeys[key])
+      throw new functions.https.HttpsError('invalid-argument', 'Invalid type of data');
+    }
+  }
+}
+
+function checkAuthentication(context) {
+  if(!context.auth){
+    throw new functions.https.HttpsError('unauthenticated', 'You must be signed to comment');
+  }
+}
